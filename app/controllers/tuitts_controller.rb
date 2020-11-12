38d6c2972fc8d4693f9991c49f32ce1b6a8c9c0e
Sergio <sergioobolevich@gmail.com>
2020-11-12 02:47:30 -0300

    Add notice/flash style and close option.

--- app/controllers/tuitts_controller.rb
+++ app/controllers/tuitts_controller.rb
@@ -1,9 +1,13 @@
 class TuittsController < ApplicationController
+
   before_action :set_article, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[new create edit update]
 
+
+
   # GET /tuitts
   # GET /tuitts.json
+
   def index
     @tuitts = Tuitt.all.order(created_at: :desc)
     @newtuitt = Tuitt.new
@@ -11,18 +15,24 @@ class TuittsController < ApplicationController
 
   # GET /tuitts/1
   # GET /tuitts/1.json
+
   def show; end
 
   # GET /tuitts/new
+
   def new
     @tuitt = Tuitt.new
   end
 
   # GET /tuitts/1/edit
-  def edit; end
+
+  def edit
+    @newtuitt = Tuitt.new
+  end
 
   # POST /tuitts
   # POST /tuitts.json
+
   def create
     @tuitt = Tuitt.new(tuitt_params)
     @tuitt.user_id = current_user.id
@@ -40,10 +50,11 @@ class TuittsController < ApplicationController
 
   # PATCH/PUT /tuitts/1
   # PATCH/PUT /tuitts/1.json
+
   def update
     respond_to do |format|
       if @tuitt.update(tuitt_params)
-        format.html { redirect_to @tuitt, notice: 'Tuitt was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Tuitt was successfully updated.' }
         format.json { render :show, status: :ok, location: @tuitt }
       else
         format.html { render :edit }
@@ -54,6 +65,7 @@ class TuittsController < ApplicationController
 
   # DELETE /tuitts/1
   # DELETE /tuitts/1.json
+
   def destroy
     @tuitt.destroy
     respond_to do |format|
@@ -61,6 +73,8 @@ class TuittsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
 
   def my_tuitts
     @tuitts = Tuitt.all.order(created_at: :desc)
@@ -70,12 +84,15 @@ class TuittsController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
+
   def set_article
     @tuitt = Tuitt.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
+
   def tuitt_params
     params.require(:tuitt).permit(:tuitt)
   end
 end
+
